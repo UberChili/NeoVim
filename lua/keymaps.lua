b9c01bdaa14d1e9d70a1c30f775997930cc750a8
Andrés <nunezlopez.ja@gmail.com>
2024-12-05 11:59:15 -0600

    added keymap to source current file

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -43,6 +43,8 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`

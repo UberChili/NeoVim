c5ab406912d1a3eca80d69cbe8a25f562f72d5b1
Andrés <nunezlopez.ja@gmail.com>
2025-01-20 11:48:31 -0600

    Installed orgmode.lua. Need to actually configure it now

---
+++ lua/custom/plugins/orgmode.lua
@@ -0,0 +1,21 @@
+-- Need to actually configure this
+
+return {
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup {
+      org_agenda_files = '~/Notes/**/*',
+      org_default_notes_file = '~/Notes/inbox.org',
+    }
+
+    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+    -- add ~org~ to ignore_install
+    -- require('nvim-treesitter.configs').setup({
+    --   ensure_installed = 'all',
+    --   ignore_install = { 'org' },
+    -- })
+  end,
+}
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -68,6 +68,7 @@ require('lazy').setup({
   require 'custom.plugins.bufferline',
   require 'custom.plugins.catpuccin',
   require 'custom.plugins.avante',
+  require 'custom.plugins.orgmode',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.

3b20b0f068938d89af9cef8325974352a60bb739
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-01-21 19:02:09 -0600

    Installed Zls for Zig integration with LSP. Still need to configure Org and Org Roam

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -202,6 +202,11 @@ return {
             },
           },
         },
+        zls = {
+          cmd = { 'zls' },
+          filetypes = { 'zig', 'zir' },
+          single_file_support = true,
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:

4d1edc5f7e02a0b8caf53db70ee686d96eb37c08
Andrés <nunezlopez.ja@gmail.com>
2025-03-08 08:54:16 -0600

    changed colorscheme

--- init.lua
+++ init.lua
@@ -22,4 +22,5 @@ require 'lazy-plugins'
 -- vim: ts=2 sts=2 sw=2 et
 --
 -- Colorscheme
-vim.cmd.colorscheme 'catppuccin-frappe'
+-- vim.cmd.colorscheme 'catppuccin-frappe'
+vim.cmd.colorscheme = function() end

f87fec0e9dd90e27d979c06bf559c40a9edccaec
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-04-03 13:45:25 -0600

    Removed shade lol

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -68,7 +68,6 @@ require('lazy').setup({
   require 'custom.plugins.bufferline',
   require 'custom.plugins.catpuccin',
   require 'custom.plugins.avante',
-  require 'custom.plugins.shade',
   -- require 'custom.plugins.orgmode',
   -- require 'custom.plugins.org-roam',
 
